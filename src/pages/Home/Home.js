7ac33004b6f00c283861d38396816afe859998c6
Owen Delisle <owen37164@gmail.com>
2019-01-28 12:24:11 -0800

    Changed stratigic sentence

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,8 +6,13 @@ function Home({ id }) {
       <div className="Home__container">
         <h1 className="heading-primary u-margin-btm-lg">Developer.</h1>
         <p className="description-text u-margin-btm-md">
-          Hi, I'm Owen! I develop state of the art mobile and web applications
-          with the latest and greatest tech.
+          I am passionate about software and enjoy learning about new languages
+          and techniques. I taught myself HTML, CSS, and JS when I was thirteen
+          and have been striving to learn more ever since. I enjoy writing clean
+          and concise algorithms to solve systematic problems. I have
+          demonstrated my ability to add features to live projects in a
+          professional environment with minimal supervision, while working
+          effectively as part of a team.
         </p>
       </div>
     </div>
